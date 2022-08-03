1b37f747e98785170f3ab0f00bc210daddf3c79f
Austin-Buchanan <austinbuchanan233@gmail.com>
2022-08-03 16:03:23 -0500

    Added authentication and tags.

--- media_ratings/app/main.py
+++ media_ratings/app/main.py
@@ -10,7 +10,7 @@ import time
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
 from .database import engine, get_db
-from .routers import videogame, user
+from .routers import videogame, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -29,6 +29,7 @@ while True:
 
 app.include_router(videogame.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 @app.get("/")
 async def root():
--- media_ratings/app/routers/videogame.py
+++ media_ratings/app/routers/videogame.py
@@ -4,14 +4,17 @@ from typing import List
 from .. import models, schemas
 from ..database import get_db
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/videogames",
+    tags="Video Games"
+)
 
-@router.get("/videogames", response_model=List[schemas.VideoGameOut])
+@router.get("/", response_model=List[schemas.VideoGameOut])
 def get_videogames(db: Session = Depends(get_db)):
     games = db.query(models.VideoGame).all()
     return games
 
-@router.post("/videogames", status_code=status.HTTP_201_CREATED, response_model=schemas.VideoGame)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.VideoGame)
 def create_videogame(game: schemas.VideoGame, db: Session = Depends(get_db)):
     new_game = models.VideoGame(**game.dict())
     db.add(new_game)
@@ -19,14 +22,14 @@ def create_videogame(game: schemas.VideoGame, db: Session = Depends(get_db)):
     db.refresh(new_game)
     return new_game
 
-@router.get("/videogames/{id}", response_model=schemas.VideoGameOut)
+@router.get("/{id}", response_model=schemas.VideoGameOut)
 def get_videogame(id: int, db: Session = Depends(get_db)):
     game = db.query(models.VideoGame).filter(models.VideoGame.id == id).first()
     if not game:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Video game with id: {id} was not found")
     return game
 
-@router.delete("/videogames/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_videogame(id: int, db: Session = Depends(get_db)):
     game = db.query(models.VideoGame).filter(models.VideoGame.id == id)
 
@@ -38,7 +41,7 @@ def delete_videogame(id: int, db: Session = Depends(get_db)):
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@router.put("/videogames/{id}", response_model=schemas.VideoGameOut)
+@router.put("/{id}", response_model=schemas.VideoGameOut)
 def update_videogame(id: int, updated_game: schemas.VideoGame, db: Session = Depends(get_db)):
     game_query = db.query(models.VideoGame).filter(models.VideoGame.id == id)
     game = game_query.first()
